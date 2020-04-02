0914a91b2e55b48331e7f442d2941578f3072299
Jacob Schwartz <jtschwartz98@gmail.com>
2020-04-02 16:57:34 -0400

    Renamed Coin and removal of Friction material

--- Assets/Game.cs
+++ Assets/Game.cs
@@ -5,7 +5,7 @@ public class Game : MonoBehaviour {
 	public GameObject coinPrefab;
 	public Slider playerControls;
 	public Text scoreBoard;
-	private int _coinCount = 750;
+	private const int CoinCount = 750;
 	private int _score = 0;
 
 	void IncreaseScore() {
@@ -15,7 +15,7 @@ public class Game : MonoBehaviour {
 	void Start() {
 		scoreBoard.text = "Score: 0";
 
-		for (int i = 0; i < _coinCount; i++) {
+		for (int i = 0; i < CoinCount; i++) {
 			Instantiate(coinPrefab,
 						new Vector3(Random.Range(-9.5f, 9.5f), 5, Random.Range(-4.5f, 5.5f)),
 						Quaternion.Euler(new Vector3(0, 0, 0)));
---
+++ README.md
@@ -0,0 +1,9 @@
+# Classic Arcade Coin Pusher
+## By: Jacob Schwartz
+
+As the title suggests, this is a simple modern take on the good old fashioned arcade game of dropping a coin into a pile of bunch more in hopes that its enough to get one of the push arms to shove even more coins over the edge.
+
+## Instructions
+1. Move the gold slider at the bottom to where about you want the coin to fall in at the back of the platform
+1. Hit the Spacebar to drop a coin once you have chosen your placement
+1. Watch the push arms go to work and see if you force any coins to fall over the edge
